9b66acf4842a68511e173e705895fafbb57ac4d0
Saumeel Gajera <SGajera@walmartlabs.com>
2016-08-08 16:20:20 -0700

    Initial Commit

---
+++ UIPageControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIPageControl
+//
+//  Created by Saumeel Gajera on 8/8/16.
+//  Copyright © 2016 walmart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
