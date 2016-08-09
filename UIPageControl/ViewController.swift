97f8951f23443ac5e2d79e73f80bc10c1bd0bfd4
Saumeel Gajera <SGajera@walmartlabs.com>
2016-08-08 17:31:15 -0700

    horizontal scroll, width customized first view

--- UIPageControl/ViewController.swift
+++ UIPageControl/ViewController.swift
@@ -8,16 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        let pageWidth = scrollView.bounds.width
+        let pageHeight = scrollView.bounds.height
+        
+        scrollView.contentSize = CGSizeMake(3*pageWidth, pageHeight)
+        scrollView.pagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = true
+        
+        let label = UILabel(frame: CGRect(x: 110, y: 10, width: pageWidth/3, height: pageHeight/10))
+        label.text = "Header Here"
+        
+        let subView = UIView(frame: CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight/3))
+        subView.backgroundColor = UIColor.lightGrayColor()
+        subView.addSubview(label)
+        
+        let view1 = UIView(frame: CGRect(x: 0,y: 0,width: pageWidth, height: pageHeight))
+        view1.backgroundColor = UIColor.blueColor()
+        view1.addSubview(subView)
+        
+        
+        let view2 = UIView(frame: CGRect(x: pageWidth,y: 0,width: pageWidth, height: pageHeight))
+        view2.backgroundColor = UIColor.redColor()
+        let view3 = UIView(frame: CGRect(x: 2*pageWidth,y: 0,width: pageWidth, height: pageHeight))
+        view3.backgroundColor = UIColor.greenColor()
+        
+        scrollView.addSubview(view1)
+        scrollView.addSubview(view2)
+        scrollView.addSubview(view3)
+        
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        
+        let page: Int = Int(round(scrollView.contentOffset.x/320))
+        
+        print(page)
+        pageControl.currentPage = page
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        print("scroll view did scroll : \(scrollView.contentOffset)")
     }
 
 
